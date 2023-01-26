bc6ec61bd30dc3a6adfd70d2334384e1d3068ae6
drkms <camille.jan@kms.io>
2023-01-26 17:05:11 +0000

    Deletion of useless includes

--- python/main.py
+++ python/main.py
@@ -1,8 +1,5 @@
-from typing import List, Dict
 from flask import Flask
-import mysql.connector
 import json
-import os
 from quotes.quotes import Quote
 
 app = Flask(__name__)
@@ -10,10 +7,7 @@ my_quote = Quote()
 
 @app.route('/')
 def index() -> str:
-    quotes = my_quote.get_quotes()
-    print(quotes)
-    quotes = my_quote.format_quotes(quotes)
-    return json.dumps(quotes)
+    return json.dumps(my_quote.format_quotes(my_quote.get_quotes()))
 
 @app.after_request
 def after_request(response):
@@ -21,7 +15,6 @@ def after_request(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     response.headers['Content-type'] = "application/json; charset=utf-8"
-    
     return response 
 
 if __name__ == '__main__':
