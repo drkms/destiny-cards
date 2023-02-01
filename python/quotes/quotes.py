1c18490c5fde2e8d86f6b61a41cbaa243db03104
drkms <camille.jan@kms.io>
2023-02-01 18:20:47 +0000

    Set fake values in case of run with pytest module

--- python/quotes/quotes.py
+++ python/quotes/quotes.py
@@ -2,15 +2,25 @@ from typing import List, Dict
 import mysql.connector
 import json
 import os
+import sys
 
 class Quote:
   def __init__(self):
-    self.config = {
-      'user': os.environ['MYSQL_USER'],
-      'password': os.environ['MYSQL_PWD'],
-      'host': os.environ['MYSQL_URL'],
-      'port': os.environ['MYSQL_PORT'],
-      'database': os.environ['MYSQL_DATABASE']
+    if "pytest" in sys.modules:
+      self.config = {
+        'user': "root",
+        'password': "root",
+        'host': "127.0.0.1",
+        'port': "3306",
+        'database': "cards"
+      }
+    else:
+      self.config = {
+        'user': os.environ.get('MYSQL_USER'),
+        'password': os.environ.get('MYSQL_PWD'),
+        'host': os.environ.get('MYSQL_URL'),
+        'port': os.environ.get('MYSQL_PORT'),
+        'database': os.environ.get('MYSQL_DATABASE')
       }
     
   def get_quotes(self) -> List[Dict]:
